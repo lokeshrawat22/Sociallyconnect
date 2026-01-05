b70e854a03013f5492d7f06d6597da22c5fdb391
Lokesh Singh Rawat <lokeshrawat0022@gmail.com>
2026-01-05 15:47:48 +0530

    Update api.jsx

--- Frontend/src/api.jsx
+++ Frontend/src/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-    baseURL : "http://localhost:3000/api"
+    baseURL : "https://sociallyconnect-g0gs.onrender.com/api"
 });
 
 API.interceptors.request.use((req)=>{
