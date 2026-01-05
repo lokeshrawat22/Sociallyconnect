8688b77ae1c73cd0b4cb0faea18de140a9238c82
Lokesh Singh Rawat <lokeshrawat0022@gmail.com>
2026-01-05 16:24:40 +0530

    Update sendEmail.js

--- Backend/utils/sendEmail.js
+++ Backend/utils/sendEmail.js
@@ -1,15 +1,18 @@
 const nodemailer = require("nodemailer");
 
-const sendEmail = async ({ to, subject, html }) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+  pool: true,             
+  maxConnections: 5,
+  maxMessages: 100,
+});
 
-  await transporter.sendMail({
+const sendEmail = async ({ to, subject, html }) => {
+  return transporter.sendMail({
     from: `"Support" <${process.env.EMAIL_USER}>`,
     to,
     subject,
