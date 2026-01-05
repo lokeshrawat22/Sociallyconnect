fdde83382f7ef82df8ff70b214f05e572fa33c46
Lokesh Singh Rawat <lokeshrawat0022@gmail.com>
2026-01-05 15:48:15 +0530

    Update socket.js

--- Frontend/src/services/socket.js
+++ Frontend/src/services/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3000", {
+const socket = io("https://sociallyconnect-g0gs.onrender.com", {
   auth: {
     token: localStorage.getItem("token"),
   },
